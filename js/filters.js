af34e63437bf81d3ed8ef154dc66a3ad37a6347f
Valor Naram <valinora@gmx.net>
2019-04-20 18:30:33 +0200

    Bugfix and see my 'Report for min_age' at #43

--- js/filters.js
+++ js/filters.js
@@ -24,16 +24,16 @@ var filter = { //The filters, the query they trigger, their names and technical 
 0: {"query": {"node|way": ["[\"healthcare:speciality\"~\"paediatrics\"]"]},  "color": profiles.redMarker, "address" : "health paediatrics"},
 1: {"query": {"node|way": ["[\"healthcare\"=\"midwife\"]"]},  "color": profiles.darkredMarker, "address" : "health midwife"},
 2: {"query": {"node|way": ["[\"healthcare\"=\"birthing_center\"]"]},  "color": profiles.lightredMarker, "address" : "health birth"},
-3: {"query": {"node|way": ["[\"leisure\"=\"playground\"]", "[\"access\"!=\"private\"]"]},  "color": profiles.greenMarker, "address" : "activity playground"},
-4: {"query": {"way|relation": ["[\"leisure\"=\"park\"]", "[\"access\"!=\"private\"]", "[\"name\"]"]},  "color": profiles.darkgreenMarker, "address" : "activity park"},
+3: {"query": {"node|way": ["[\"leisure\"=\"playground\"]", "[\"access\"!=\"private\"]", "[\"min_age\"!~\"[4-99]\"]"]},  "color": profiles.greenMarker, "address" : "activity playground"},
+4: {"query": {"way|relation": ["[\"leisure\"=\"park\"]", "[\"access\"!=\"private\"]", "[\"name\"]", "[\"min_age\"!~\"[4-99]\"]"]},  "color": profiles.darkgreenMarker, "address" : "activity park"},
 5: {"query": {"node|way": ["[\"shop\"=\"baby_goods\"]"]},  "color": profiles.blueMarker, "address" : "shop baby_goods"},
 6: {"query": {"node|way": ["[\"shop\"=\"toys\"]"]},  "color": profiles.darkblueMarker, "address" : "shop toys"},
 7: {"query": {"node|way": ["[\"shop\"=\"clothes\"]", "[\"clothes\"~\"babies|children\"]"]},  "color": profiles.lightblueMarker, "address" : "shop clothes"},
 8: {"query": {"node|way": ["[\"amenity\"~\"kindergarten|childcare\"]"]},  "color": profiles.orangeMarker, "address" : "childcare kindergarten"},
 9: {"query": {"node|way": ["[\"tourism\"=\"zoo\"]"]},  "color": profiles.yellowMarker, "address" : "activity zoo"},
 10: {"query": {"node|way": ["[\"diaper\"]", "[\"diaper\"!=\"no\"]"]},  "color": profiles.lightgreyMarker, "address" : "childcare diaper"},
-11: {"query": {"node|way": ["[\"amenity\"=\"cafe\"]"]},  "color": profiles.violetMarker, "address" : "eat cafe"},
-12: {"query": {"node|way": ["[\"amenity\"=\"restaurant\"]"]},  "color": profiles.lightvioletMarker, "address" : "eat restaurant"}
+11: {"query": {"node|way": ["[\"amenity\"=\"cafe\"]", , "[\"min_age\"!~\"[4-99]\"]"]},  "color": profiles.violetMarker, "address" : "eat cafe"},
+12: {"query": {"node|way": ["[\"amenity\"=\"restaurant\"]", , "[\"min_age\"!~\"[4-99]\"]"]},  "color": profiles.lightvioletMarker, "address" : "eat restaurant"}
 };
 function triggerActivationOfFilters() {
 	clearTimeout(timerForFilter);
